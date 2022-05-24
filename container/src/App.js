acd65097be6bbe09e671c1493dd5962a98007e41
deverober <robthedeveloper@icloud.com>
2022-05-24 09:17:01 +0200

    small refactor

--- container/src/App.js
+++ container/src/App.js
@@ -1,12 +1,12 @@
 import React from 'react'
 import MarketingApp from './components/MarketingApp'
 
-console.log('TEST');
-
-export default () => {
+const App = () => {
     return <div>
         <h1>Hi there!</h1>
         <hr />
         <MarketingApp />
     </div>
 }
+
+export default App
