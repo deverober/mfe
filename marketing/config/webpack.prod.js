ef1815ea754613f55a007099f4bb43f4f7466a7c
deverober <robthedeveloper@icloud.com>
2022-05-25 13:38:12 +0200

    another configuration error

--- marketing/config/webpack.prod.js
+++ marketing/config/webpack.prod.js
@@ -6,7 +6,7 @@ const commonConfig = require('./webpack.common')
 const prodConfig = {
     mode: 'production',
     output: {
-        filename: '[name].[contenthash].js',
+        filename: 'remoteEntry.js',
         publicPath: '/marketing/latest/'
     },
     plugins: [
